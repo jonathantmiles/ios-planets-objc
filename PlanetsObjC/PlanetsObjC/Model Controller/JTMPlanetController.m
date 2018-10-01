71cf6dceb6cdc8ab023c9630768aad4f8db97f39
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-01 13:59:28 -0700

    model controllers have working arrays

--- PlanetsObjC/PlanetsObjC/Model Controller/JTMPlanetController.m
+++ PlanetsObjC/PlanetsObjC/Model Controller/JTMPlanetController.m
@@ -8,6 +8,7 @@
 
 #import "JTMPlanetController.h"
 #import "JTMPlanet.h"
+#import <UIKit/UIKit.h>
 
 @implementation JTMPlanetController
 
@@ -16,8 +17,27 @@
     self = [super init];
     if (self) {
         _planetsWithoutPluto = [[NSArray alloc] initWithObjects:
-                                [[JTMPlanet alloc] initWithPlanetName:@"Mercury" image:mercury.png],
-                                nil]
+                                [[JTMPlanet alloc] initWithPlanetName:@"Mercury" image:[UIImage imageNamed:@"mercury.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Venus" image:[UIImage imageNamed:@"venus.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Earth" image:[UIImage imageNamed:@"earth.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Mars" image:[UIImage imageNamed:@"mars.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Jupiter" image:[UIImage imageNamed:@"jupiter.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Saturn" image:[UIImage imageNamed:@"saturn.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Uranus" image:[UIImage imageNamed:@"uranus.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Neptune" image:[UIImage imageNamed:@"neptune.png"]],
+                                nil];
+        _planetsWithPluto = [[NSArray alloc] initWithObjects:
+                                [[JTMPlanet alloc] initWithPlanetName:@"Mercury" image:[UIImage imageNamed:@"mercury.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Venus" image:[UIImage imageNamed:@"venus.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Earth" image:[UIImage imageNamed:@"earth.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Mars" image:[UIImage imageNamed:@"mars.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Jupiter" image:[UIImage imageNamed:@"jupiter.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Saturn" image:[UIImage imageNamed:@"saturn.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Uranus" image:[UIImage imageNamed:@"uranus.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Neptune" image:[UIImage imageNamed:@"neptune.png"]],
+                                [[JTMPlanet alloc] initWithPlanetName:@"Pluto" image:[UIImage imageNamed:@"pluto.png"]],
+                                nil];
+        
     }
     return self;
 }
